f2d28707c7965e776c9431bfd4bbeee0c2ab6e2d
ayallau <ayallau@gmail.com>
2025-09-12 23:56:16 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+// const { request } = require("express");
+import { express } from "express";
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send({ Hi: "there" });
+});
+
+app.listen(5000);
